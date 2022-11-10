d2cdb3581eeba5232b664c9f02b8174eeb81ed28
Gen Graphic <117539258+GenGraphic@users.noreply.github.com>
2022-11-10 06:29:04 +0100

    small changes
    footer link
    added menu elements

---
+++ main.js
@@ -0,0 +1,13 @@
+const facebookLink = document.getElementById('facebookLink');
+const linkedin = document.getElementById('linkedinLink');
+const upworkLink = document.getElementById('upworkLink');
+
+facebookLink.addEventListener('click', () => {
+    location.href = 'https://www.facebook.com/profile.php?id=100087527200353';
+});
+linkedin.addEventListener('click', () => {
+    location.href = 'https://www.linkedin.com/in/gen-graphic-b10152256/';
+});
+upworkLink.addEventListener('click', () => {
+    location.href = 'https://www.upwork.com/freelancers/~017e3bb98e715de75a';
+});
