8cdebdfb9ac0176395bd0f922516c57c5f71818e
Gen Graphic <117539258+GenGraphic@users.noreply.github.com>
2022-11-11 21:54:59 +0100

    working on menu show and hide
    i m still trying to make the menu to hide and show

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const facebookLink = document.getElementById('facebookLink');
 const linkedin = document.getElementById('linkedinLink');
 const upworkLink = document.getElementById('upworkLink');
+const menuIcon = document.getElementById('menuIcon');
+const menuOptions = document.getElementById('menuOptions');
 
+//making buttons fot footer
 facebookLink.addEventListener('click', () => {
     location.href = 'https://www.facebook.com/profile.php?id=100087527200353';
 });
@@ -10,4 +13,18 @@ linkedin.addEventListener('click', () => {
 });
 upworkLink.addEventListener('click', () => {
     location.href = 'https://www.upwork.com/freelancers/~017e3bb98e715de75a';
+});
+
+//making button for menu
+menuIcon.addEventListener('click', () => {
+    if (menuOptions.style.display = 'none')
+    {
+        menuOptions.style.display = 'block';
+        menuIcon.src = 'images/exit.png';
+        console.log('display');
+    }else if(menuIcon.src = 'images/exit.png') {
+        menuOptions.style.display = 'none';
+        menuIcon.src = 'images/menuIcon.png';
+        console.log('hide');
+    }
 });
