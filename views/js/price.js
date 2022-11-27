2fd2789720d84d4998a841f9fbeaf23977bb96ba
Gen Graphic <117539258+GenGraphic@users.noreply.github.com>
2022-11-27 17:10:01 +0100

    done the prices page
    i still have to make is responsive

---
+++ views/js/price.js
@@ -0,0 +1,80 @@
+var packageToCustomize = document.getElementById('package_to_customize');
+
+//replace the div of package to customize with the one is clicked
+document.addEventListener('click', function(e) {
+    if (e.target.id == "customizeBronzeBtn") {
+        packageToCustomize.innerHTML = document.getElementById('bronze_package').innerHTML;
+        RemoveAllFeature();
+    }
+    if (e.target.id == "customizeBusinessBtn") {
+        packageToCustomize.innerHTML = document.getElementById('business_package').innerHTML;
+        RemoveAllFeature();
+    }
+    if (e.target.id == "customizePremiumBtn") {
+        packageToCustomize.innerHTML = document.getElementById('premium_package').innerHTML;
+        RemoveAllFeature();
+    }
+}, false);
+
+function Add_Take(obj) {
+
+    if( !(obj.classList.contains('active')) ) {
+        var parrentAbsolute = obj.parentElement.parentElement.parentElement.parentElement;
+        parrentAbsolute.classList.add('activeFeature');
+        obj.classList.add('active');
+        obj.removeAttribute('unchecked');
+        obj.setAttribute('checked', '');
+
+        //prepare the preis ans in int to be calulated
+        var preisFeature = parrentAbsolute.children[1].children[0].children[0].children[0].innerHTML;
+        const preis = preisFeature.slice(0,-1).replace('.', '');//removes last element € and the point
+
+        //function to calculate
+        function CalculatorAdd() {
+            var packagePreisElement = document.getElementById('package_to_customize').children[3].children[0].innerHTML;
+            var packagePreis = packagePreisElement.replace(/\D/g, '');
+
+            var preisToShow = Number(packagePreis) + Number(preis) + '€';
+            document.getElementById('package_to_customize').children[3].children[0].innerHTML ='New: ' + preisToShow;
+
+        }
+        CalculatorAdd();
+    }else if(obj.classList.contains('active')) {
+        var parrentAbsolute = obj.parentElement.parentElement.parentElement.parentElement;
+        parrentAbsolute.classList.remove('activeFeature');
+        obj.classList.remove('active');
+        obj.removeAttribute('checked');
+        obj.setAttribute('unchecked', '');
+    
+        //prepare the preis ans in int to be calulated
+        var preisFeature = parrentAbsolute.children[1].children[0].children[0].children[0].innerHTML;
+        const preis = preisFeature.slice(0,-1).replace('.', '');//removes last element € and the point
+    
+        //function to calculate
+        function CalculatorAdd() {
+            var packagePreisElement = document.getElementById('package_to_customize').children[3].children[0].innerHTML;
+            var packagePreis = packagePreisElement.replace(/\D/g, '');
+    
+            var preisToShow = Number(packagePreis) - Number(preis) + '€';
+            document.getElementById('package_to_customize').children[3].children[0].innerHTML ='New: ' + preisToShow;
+        }
+        CalculatorAdd();    
+        };
+}
+
+function RemoveAllFeature() {
+    var toggleList = document.querySelectorAll('input.toggle');
+    var featuresList = document.querySelectorAll('.activeFeature');
+    
+    for(var a = 0; a < featuresList.length; a++) {
+        featuresList[a].classList.remove('activeFeature');
+    }
+
+    for(var b = 0; b < toggleList.length; b++) {
+        toggleList[b].classList.remove('active');
+        toggleList[b].removeAttribute('checked');
+        toggleList[b].setAttribute('unchecked', '');
+        $('.toggle').prop('checked',false).change(); 
+    }
+}
+
