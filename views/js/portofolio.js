04f7cf37fe41beb4d1b83ee8b41b276e122145c6
Gen Graphic <117539258+GenGraphic@users.noreply.github.com>
2022-12-16 17:59:56 +0100

    portofolio page

---
+++ views/js/portofolio.js
@@ -0,0 +1,31 @@
+//const obj for the footer media links
+const facebookLink = document.getElementById('facebookLink');
+const linkedin = document.getElementById('linkedinLink');
+const upworkLink = document.getElementById('upworkLink');
+//const for the menu
+const menuIcon = document.getElementById('menuIcon');
+const menuOptions = document.getElementById('responsiveMenu');
+var menuToggle = false;
+
+//making buttons for footer
+facebookLink.addEventListener('click', () => {
+    window.open('https://www.facebook.com/profile.php?id=100087527200353', '_blank').focus();
+});
+linkedin.addEventListener('click', () => {
+    window.open('https://www.linkedin.com/in/gen-graphic-b10152256/', '_blank').focus();
+});
+upworkLink.addEventListener('click', () => {
+    window.open('https://www.upwork.com/freelancers/~017e3bb98e715de75a', '_blank').focus();
+});
+
+//making button for menu
+menuIcon.addEventListener('click', () => { 
+    if (!menuToggle)
+    {
+        menuOptions.style.height = 'auto';
+        menuToggle = true;
+    }else if(menuToggle) {
+        menuOptions.style.height = '0';
+        menuToggle = false;
+    }
+});
