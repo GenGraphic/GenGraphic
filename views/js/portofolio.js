d1bcb9f2aad9f40a2a50245a02409823720330c3
Gen Graphic <117539258+GenGraphic@users.noreply.github.com>
2022-12-17 08:47:26 +0100

    portofolio page images and texts

--- views/js/portofolio.js
+++ views/js/portofolio.js
@@ -29,3 +29,94 @@ menuIcon.addEventListener('click', () => {
         menuToggle = false;
     }
 });
+
+//Make portofolio fotos to move
+
+const arrow_up_caffebar = document.getElementById('arrow_up_caffebar');
+const arrow_up_hasse = document.getElementById('arrow_up_hasse');
+const arrow_up_pasiLumina = document.getElementById('arrow_up_pasiLumina');
+const arrow_up_weBrand = document.getElementById('arrow_up_weBrand');
+
+arrow_up_caffebar.addEventListener('click', () => {
+    const fostosCont = document.getElementById('imagesCaffeBar');
+    var list_fotos = fostosCont.children; //take all childrens from container
+
+    var firstElement = list_fotos[0];
+    //removing and adding classes for first element to show at the end
+    firstElement.classList.remove('img1');
+    firstElement.classList.add('img3');
+
+    list_fotos[0].parentNode.removeChild(list_fotos[0]);
+
+    //change classes of img to show in right order
+    list_fotos[0].classList.remove('img2');
+    list_fotos[0].classList.add('img1');
+
+    list_fotos[1].classList.remove('img3');
+    list_fotos[1].classList.add('img2');
+
+    list_fotos[0].parentNode.appendChild(firstElement);
+});
+
+arrow_up_hasse.addEventListener('click', () => {
+    const fostosCont = document.getElementById('imagesHasse');
+    var list_fotos = fostosCont.children; //take all childrens from container
+
+    var firstElement = list_fotos[0];
+    //removing and adding classes for first element to show at the end
+    firstElement.classList.remove('img1');
+    firstElement.classList.add('img3');
+
+    list_fotos[0].parentNode.removeChild(list_fotos[0]);
+
+    //change classes of img to show in right order
+    list_fotos[0].classList.remove('img2');
+    list_fotos[0].classList.add('img1');
+
+    list_fotos[1].classList.remove('img3');
+    list_fotos[1].classList.add('img2');
+
+    list_fotos[0].parentNode.appendChild(firstElement);
+});
+
+arrow_up_pasiLumina.addEventListener('click', () => {
+    const fostosCont = document.getElementById('imagesPasiLumina');
+    var list_fotos = fostosCont.children; //take all childrens from container
+
+    var firstElement = list_fotos[0];
+    //removing and adding classes for first element to show at the end
+    firstElement.classList.remove('img1');
+    firstElement.classList.add('img3');
+
+    list_fotos[0].parentNode.removeChild(list_fotos[0]);
+
+    //change classes of img to show in right order
+    list_fotos[0].classList.remove('img2');
+    list_fotos[0].classList.add('img1');
+
+    list_fotos[1].classList.remove('img3');
+    list_fotos[1].classList.add('img2');
+
+    list_fotos[0].parentNode.appendChild(firstElement);
+});
+
+arrow_up_weBrand.addEventListener('click', () => {
+    const fostosCont = document.getElementById('imagesWeBrand');
+    var list_fotos = fostosCont.children; //take all childrens from container
+
+    var firstElement = list_fotos[0];
+    //removing and adding classes for first element to show at the end
+    firstElement.classList.remove('img1');
+    firstElement.classList.add('img3');
+
+    list_fotos[0].parentNode.removeChild(list_fotos[0]);
+
+    //change classes of img to show in right order
+    list_fotos[0].classList.remove('img2');
+    list_fotos[0].classList.add('img1');
+
+    list_fotos[1].classList.remove('img3');
+    list_fotos[1].classList.add('img2');
+
+    list_fotos[0].parentNode.appendChild(firstElement);
+});
